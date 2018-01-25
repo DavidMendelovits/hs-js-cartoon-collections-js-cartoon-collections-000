ec8c780e49ed374fada72e1fe145f337e5161fa2
David Mendelovits <david.mendelovits@gmail.com>
2018-01-25 04:34:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 function dwarfRollCall(dwarves) {
+  let rollCall = [];
+  for (let i = 0; i < dwarves.length; i++) {
+    rollCall.push(`${i+1}. ${dwarves[i]}`)
+  }
+  return rollCall.join(" ") + " "
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var PlanetCall = [];
+  for (let i = 0; i < planeteerCalls.length; i++){
+    PlanetCall.push (`${planeteerCalls[i].toUpperCase()} + !`)
+  }
+  return PlanetCall
 }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i < words.length; i++){
+    if (words[i].length > 4) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function findTheCheese (foods) {
